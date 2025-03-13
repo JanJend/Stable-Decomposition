72514087e9e76c436aafd7552e5ad5eeb4a84913
Jan Jendrysiak <Jan@Jendrysiak.org>
2025-03-13 19:59:27 +0000

    basic structure written

---
+++ pruning.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file pruning.cpp
+ * @author Jan Jendrysiak and Fabian Lenzen
+ * @brief 
+ * @version 0.1
+ * @date 2025-03-13
+ * 
+ * @copyright 
+ * 
+ */
+
+#ifndef PRUNING_CPP
+#define PRUNING_CPP
+
+#include <pruning_interface.hpp>
+
+namespace stable_decomposition {
+
+} // namespace stable_decomposition
+
+#endif // PRUNING_CPP
+
---
+++ pruning.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file pruning.hpp
+ * @author Jan Jendrysiak and Fabian Lenzen
+ * @brief 
+ * @version 0.1
+ * @date 2025-03-13
+ * 
+ * @copyright 
+ * 
+ */
+
+#ifndef PRUNING_HPP
+#define PRUNING_HPP
+
+#include <grlina/graded_linalg.hpp>
+
+namespace stable_decomposition {
+
+using namespace graded_linalg;
+using index = int; // Change to large enough int type.
+
+
+
+
+} // namespace stable_decomposition
+
+#endif // PRUNING_HPP
